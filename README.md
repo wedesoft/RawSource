86905944c52cc3f0cc4ca41ad4c01e4a884537c5
Alexandre Girard <git@alexgirard.com>
2014-09-06 16:31:33 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-RawSource
-=========
+CumulusClips Plugin : RawSource
+===============================
 
-CumulusClips Plugin : RawSource - keep raw file after encoding
+Keep raw video file in **UPLOAD_PATH + '/raw'** folder after the encoding process.
+
+# Install
+
+1. Go to your CumulusClips plugin folder
+2. ```git clone https://github.com/alx/RawSource.git```
+3. In CumulusClips admin interface, enable RawSource plugin from plugins panel
---
+++ RawSource.php
@@ -0,0 +1,36 @@
+<?php
+class RawSource {
+
+    static function Load() {
+        Plugin::Attach ( 'encode.before_flv_encode' , array( __CLASS__ , 'KeepRawFile' ) );
+    }
+
+    static function Info() {
+        return array (
+            'name'    => 'RawSource',
+            'author'  => 'Alexandre Girard',
+            'version' => '1.0.0',
+            'site'    => 'http://alexgirard.com/',
+            'notes'   => 'Keep uploaded video file in UPLOAD_PATH/raw/ folder'
+        );
+    }
+
+    static function Install() {
+    }
+
+    static function Uninstall() {
+    }
+
+    static function KeepRawFile() {
+      App::LoadClass ('Video');
+      global $video;
+
+      $src_video = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->original_extension;
+      $raw_video = UPLOAD_PATH . '/raw/' . $video->filename . '.' . $video->original_extension;
+
+      Filesystem::Open();
+      Filesystem::Copy($src_video, $raw_video);
+      Filesystem::Close();
+    }
+}
+?>
